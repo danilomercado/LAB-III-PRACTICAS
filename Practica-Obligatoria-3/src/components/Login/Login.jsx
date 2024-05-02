3ee9d4654613cd9e279b6370c956ac9a2c06e62b
Danilo Mercado <mercadodanilo213@hotmail.com>
2024-05-02 18:26:41 -0300

    Practica Obligatoria 3

--- Practica-Obligatoria-3/src/components/Login/Login.jsx
+++ Practica-Obligatoria-3/src/components/Login/Login.jsx
@@ -1,30 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [inputPre, setInputPre] = useState("");
+  const verf = inputPre.includes("o") || inputPre.includes("O");
+  const handlerInputPreview = (event) => {
+    setInputPre(event.target.value);
+    if (verf) {
+      alert("Por favor, ¡Nombres de usuario sin la letra o!");
+    }
+  };
+
+  const verifyRegister = () => {
+    if (inputPre === "" || verf) {
+      alert("Usuario inválido para registrarse");
+    } else {
+      alert("¡Usuario registrado correctamente!");
+    }
+  };
   return (
-    <div className="input-group mb-3">
-      <input
-        type="text"
-        className="form-control"
-        placeholder="Recipient's username"
-        aria-label="Recipient's username"
-        aria-describedby="button-addon2"
-      />
-      <input
-        type="text"
-        className="form-control"
-        placeholder="Recipient's username"
-        aria-label="Recipient's username"
-        aria-describedby="button-addon2"
-      />
-      <button
-        className="btn btn-outline-primary black"
-        type="button"
-        id="button-addon2"
-      >
-        Login
-      </button>
-    </div>
+    <>
+      <div className="mb-3">
+        <form className="input-group mb-3 justify-content-center">
+          <input type="text" onChange={handlerInputPreview} value={inputPre} />
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={verifyRegister}
+          >
+            Registrarse
+          </button>
+          <p className="input-group mb-3 justify-content-center">{inputPre}</p>
+        </form>
+      </div>
+    </>
   );
 };
 
